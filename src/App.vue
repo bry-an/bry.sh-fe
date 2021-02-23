394e94f3fabb9d2d4e447a35765dea6dfdeed4ec
Bryan Yunis <bryany@booj.com>
2021-02-22 22:19:01 -0500

    inital commit, bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app-wrapper">
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=PT+Mono&display=swap');
+@import '@/assets/reset.scss';
+@import '@/assets/bry.scss';
+
+body {
+  box-sizing: border-box;
+  background-color: $dark-purple;
+}
+.app-wrapper {
+  width: 80%;
+  margin: auto;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">
+      <h1># bry.sh #</h1>
+      <h3>Custom URL shortener</h3>
+    </div>
+    <div class="input">
+      <p>
+        <label for="url-input"> Enter a URL to shorten </label>
+        <input
+          id="url-input"
+          placeholder="www.reallylongwebsite.com"
+          type="text"
+          @keyup.enter="submit"
+        >
+        <button
+          class="submit-button"
+          @click="submit"
+        >
+          Shorten
+        </button>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+  components: {},
+}
 </script>
+<style lang="scss" scoped>
+@import "@/assets/bry.scss";
+
+.home {
+  font-family: "PT Mono", monospace;
+  color: $light-green;
+}
+.input {
+  margin-top: 2rem;
+
+  #url-input {
+    margin-top: 0.5rem;
+    width: 50%;
+    height: 2rem;
+    font-size: 1.5rem;
+    font-family: "PT Mono", monospace;
+    border-radius: 3px;
+  }
+}
+.submit-button {
+  background-color: $light-green;
+  border: 1px solid $dark-purple;
+  height: 2.2rem;
+  margin-left: 1rem;
+  cursor: pointer;
+  border-radius: 5px;
+  color: $dark-purple;
+  font-family: "PT mono", monospace;
+  font-size: 1.16rem;
+}
+
+.title {
+  text-align: center;
+}
+label {
+  display: block;
+}
+</style>
